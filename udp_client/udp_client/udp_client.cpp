883c1ace45cd499e2c1be0e294b77f40eb33ae3e
666egert <egert20@hotmail.com>
2019-06-01 00:43:51 +0300

    Added secont timer for standard output

--- udp_client/udp_client/udp_client.cpp
+++ udp_client/udp_client/udp_client.cpp
@@ -9,7 +9,6 @@
 #include <ctime>  
 
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/bind.hpp>
 #include <boost/asio/ip/address_v4.hpp>
@@ -23,7 +22,6 @@ using boost::asio::ip::udp;
 std::string get_timestamp()
 {
 	using namespace std; // For time_t, time and ctime;
-	//time_t now = time(0);
 	return std::to_string(time(0));
 }
 
@@ -47,8 +45,10 @@ private:
 class udp_client {
 public:
 	udp_client(boost::asio::io_context& io)
-	:	socket_(io), 
-		rx_timer_(io, boost::asio::chrono::milliseconds(10)),
+	:	strand_(io),
+		socket_(io),
+		rx_timer_(io, boost::posix_time::milliseconds(100)),
+		print_timer_(io, boost::posix_time::seconds(10)),
 		file_() {
 		
 		socket_.open(udp::v4());
@@ -56,13 +56,20 @@ public:
 		socket_.set_option(option);
 		socket_.bind(udp::endpoint(boost::asio::ip::address_v4::address_v4::any(), 12345));
 
-		rx_timer_.async_wait(boost::bind(&udp_client::upd_cliend_rx, this));
+		rx_timer_.async_wait(strand_.wrap(boost::bind(&udp_client::upd_client_rx, this)));
+		print_timer_.async_wait(strand_.wrap(boost::bind(&udp_client::udp_print, this)));
 	}
 
-	void upd_cliend_rx(void) {
+	void udp_print() {
+		std::cout << "Udp_print\r\n";
+
+		print_timer_.expires_at(print_timer_.expires_at() + boost::posix_time::seconds(10));
+		print_timer_.async_wait(strand_.wrap(boost::bind(&udp_client::udp_print, this)));
+	}
+
+	void upd_client_rx(void) {
 		boost::array<char, 128> recv_buf;
 		udp::endpoint sender_endpoint;
-		std::cout << "Recv\r\n";
 		size_t len = socket_.receive_from(
 			boost::asio::buffer(recv_buf), sender_endpoint);
 		if (len > 0) {
@@ -70,20 +77,19 @@ public:
 			value_type_ = recv_buf.data()[1];
 			sensor_data_ = (uint64_t)recv_buf.data()[2] 
 							|  (recv_buf.data()[3] << 8) 
-							| (recv_buf.data()[3] << 16) 
+							| (recv_buf.data()[4] << 16) 
 							| (recv_buf.data()[5] << 24) 
 							| (recv_buf.data()[6] << 32)
 							| (recv_buf.data()[7] << 40)
 							| (recv_buf.data()[8] << 48)
 							| (recv_buf.data()[9] << 56);
 
-			
-			//printf("%d - %d - %ull - %s\r\n ", sensor_number_, value_type_, sensor_data_, get_timestamp());
 			file_.csv_writer(get_timestamp(),
 							std::to_string(sensor_number_), 
 							std::to_string(sensor_data_));
 		}
-		rx_timer_.async_wait(boost::bind(&udp_client::upd_cliend_rx, this));
+		rx_timer_.expires_at(rx_timer_.expires_at() + boost::posix_time::milliseconds(100));
+		rx_timer_.async_wait(strand_.wrap(boost::bind(&udp_client::upd_client_rx, this)));
 	}
 
 	~udp_client() {
@@ -91,7 +97,9 @@ public:
 	}
 private:
 	udp::socket socket_;
-	boost::asio::steady_timer rx_timer_;
+	boost::asio::io_context::strand strand_;
+	boost::asio::deadline_timer rx_timer_;
+	boost::asio::deadline_timer print_timer_;
 	uint8_t sensor_number_;
 	uint8_t value_type_;
 	uint64_t sensor_data_;
@@ -103,9 +111,9 @@ int main() {
 	try {
 		boost::asio::io_context io;
 		udp_client client(io);
+		boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
 		io.run();
-		std::cout << "END\r\n";
-
+		t.join();
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
